d0e38166bd35ab18b0ab8edea2d3af3ad9e4d87d
EthanBlack_programming <eblack@ucsd.edu>
2022-05-07 17:01:43 -0700

    #23 dunno why this was empty. fixed

--- app/src/main/java/com/example/zookeeper/RouteExhibitItem.java
+++ app/src/main/java/com/example/zookeeper/RouteExhibitItem.java
@@ -1,1 +1,20 @@
 
+package com.example.zookeeper;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+
+public class RouteExhibitItem implements Serializable {
+    public int id = 0;
+    public String name;
+    public double distance;
+    public ArrayList<String> directions;
+
+    RouteExhibitItem(String name, double distance , ArrayList<String> directions){
+        this.name = name;
+        this.distance = distance;
+        this.directions = directions;
+    }
+
+
+}
