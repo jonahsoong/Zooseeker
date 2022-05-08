01329d4c08e20a84313326efdc3ca78809224180
EthanBlack_programming <eblack@ucsd.edu>
2022-05-07 19:25:19 -0700

    #23 need to commit these before merging misc.xml and PathGeneratorTest.java

--- app/src/androidTest/java/com/example/zookeeper/PathGeneratorTest.java
+++ app/src/androidTest/java/com/example/zookeeper/PathGeneratorTest.java
@@ -15,6 +15,7 @@ import androidx.test.platform.app.InstrumentationRegistry;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class PathGeneratorTest {
     Context appContext = null;
@@ -28,26 +29,44 @@ public class PathGeneratorTest {
     public void correctPlanDuplicate(){
         ArrayList<String> input = new ArrayList<>(Arrays.asList("lions","gorillas", "gorillas", "gators","arctic_foxes","gorillas"));
         path.generatePlan(input);
-        String[] result = path.getOrder();
-        double[] distances = path.getDist();
+        List<RouteExhibitItem> result = path.getRoute();
+        ArrayList<String> names= new ArrayList<>();
+        ArrayList<Double> distances = new ArrayList<>();
+        for(RouteExhibitItem i : result){
+            if(i.name != "Entrance Gate"){
+                names.add(i.name);
+                distances.add(i.distance);
+            }
+        }
+        String[] namesR = names.stream().toArray(String[]::new);
+        double[] distancesR = distances.stream().mapToDouble(n->n).toArray();
         double[] exDist = {110,200,200,500};
         String[] expected = {"Alligators", "Lions", "Gorillas", "Arctic Foxes"};
-        Assert.assertArrayEquals(expected, result);
-        for(int i = 0; i < exDist.length; i ++){
-            Assert.assertTrue(exDist[i] == distances[i]);
+        Assert.assertArrayEquals(expected, namesR);
+        for(int k = 0; k < exDist.length; k ++){
+            Assert.assertTrue(exDist[k] == distancesR[k]);
         }
     }
     @Test
     public void linearPlan(){
         ArrayList<String> input = new ArrayList<>(Arrays.asList("gorillas","lions","elephant_odyssey"));
         path.generatePlan(input);
-        String[] result = path.getOrder();
-        double[] distances = path.getDist();
+        List<RouteExhibitItem> result = path.getRoute();
+        ArrayList<String> names= new ArrayList<>();
+        ArrayList<Double> distances = new ArrayList<>();
+        for(RouteExhibitItem i : result){
+            if(i.name != "Entrance Gate"){
+                names.add(i.name);
+                distances.add(i.distance);
+            }
+        }
+        String[] namesR = names.stream().toArray(String[]::new);
+        double[] distancesR = distances.stream().mapToDouble(n->n).toArray();
         String[] expected = {"Gorillas", "Lions", "Elephant Odyssey"};
         double[] exDist = {210,200,200};
-        Assert.assertArrayEquals(expected, result);
-        for(int i = 0; i < exDist.length; i ++){
-            Assert.assertTrue(exDist[i] == distances[i]);
+        Assert.assertArrayEquals(expected, namesR);
+        for(int k = 0; k < exDist.length; k ++){
+            Assert.assertTrue(exDist[k] == distancesR[k]);
         }
 
     }
